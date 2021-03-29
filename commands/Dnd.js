476a11892a0ae4ac148dd36cc4028e23197a5ddb
deja <26201406+iamdeja@users.noreply.github.com>
2021-03-29 19:39:26 +0300

    Separate concerns for dnd command

--- commands/Dnd.js
+++ commands/Dnd.js
@@ -3,83 +3,48 @@ import Pc from "../models/dndPc.js";
 import { getUserFromLink } from "./helpers/guildFunctions.js";
 import { MessageEmbed } from "discord.js";
 
-const handlePC = async (message, args) => {
-  if (!args[1]) {
-    return message.channel.send("Wrong usage. Help coming when I'm not lazy.");
+const fixedResponses = {
+  pcCallError:
+    "Wrong command usage. Correct usage is `dnd pc set --id player-nick [arguments]`.",
+  pcArgumentParseError: "Could not parse arguments. Please check your syntax.",
+  pcUpdateSuccess: "PC successfully updated. View using `dnd player-nick`.",
+  pcUpdateFailure: "Something went wrong. The PC could not be updated.",
+};
+
+const handlePCUpdate = async (message, args) => {
+  if (!args[1] || args[1] !== "set")
+    return message.channel.send(fixedResponses.pcCallError);
+  args = args.slice(2);
+
+  const inputOptions = {};
+  let error = false;
+  for (let i = 0; i < args.length; i += 2) {
+    const arg = args[i];
+    if (arg[0] !== "-" && arg[1] !== "-") {
+      error = true;
+      break;
+    }
+    inputOptions[arg.slice(2)] = args[i + 1].replace(/_/g, " ");
   }
 
-  if (args[1] === "set") {
-    args = args.slice(2);
-    const options = {};
-    let error = false;
+  if (error) return message.channel.send(fixedResponses.pcArgumentParseError);
 
-    for (let i = 0; i < args.length; i += 2) {
-      const arg = args[i];
-      if (arg[0] !== "-" && arg[1] !== "-") {
-        error = true;
-        break;
-      }
-      options[arg.slice(2)] = args[i + 1].replace(/_/g, " ");
-    }
+  let userId;
+  if (inputOptions.hasOwnProperty("id")) {
+    userId = await getUserFromLink(inputOptions.id.toLowerCase());
+    if (!userId) return message.channel.send("No user with this id found.");
+  } else return message.channel.send("You must specify a user id.");
 
-    if (error) {
-      return message.channel.send(
-        "Could not parse arguments. Please recheck your syntax!"
-      );
-    }
+  // Suffix to handle multiple PCs for future campaigns.
+  userId += "_1";
 
-    let userId;
-
-    if (options.hasOwnProperty("id")) {
-      userId = await getUserFromLink(options.id.toLowerCase());
-      if (!userId) return message.channel.send("No user with this id found.");
-    } else return message.channel.send("You must specify an user id.");
-
-    userId += "_1";
-
-    const mongoOptions = {};
-
-    for (let [key, value] of Object.entries(options)) {
-      /*if (key !== "bio") {
-              value = value.toLowerCase();
-              value = value.charAt(0).toUpperCase() + value.slice(1);
-            }*/
-
-      switch (key) {
-        case "name":
-          mongoOptions.name = value;
-          break;
-        case "race":
-          mongoOptions.race = value;
-          break;
-        case "class":
-          mongoOptions.class = value;
-          break;
-        case "pictureURL":
-          mongoOptions.pictureURL = value;
-          break;
-        case "bio":
-          mongoOptions.bio = value;
-          break;
-      }
-    }
-
-    try {
-      await Pc.findByIdAndUpdate(
-        userId,
-        { $set: mongoOptions },
-        { upsert: true }
-      ).exec();
-      return message.channel.send(
-        "PC successfully updated. View using `dnd id`."
-      );
-    } catch (e) {
-      console.error(e);
-      return message.channel.send(
-        "Something went wrong. The character could not be updated."
-      );
-    }
-  }
+  const mongoOptions = formatForMongoQuery(inputOptions);
+  const updateResult = await updatePc(userId, mongoOptions);
+  message.channel.send(
+    updateResult
+      ? fixedResponses.pcUpdateSuccess
+      : fixedResponses.pcUpdateFailure
+  );
 };
 
 export default class Dnd extends Command {
@@ -94,7 +59,7 @@ export default class Dnd extends Command {
     switch (args[0]) {
       case "pc":
       case "PC":
-        await handlePC(message, args);
+        await handlePCUpdate(message, args);
         break;
       default:
         const userId = await getUserFromLink(args[0]);
@@ -110,9 +75,8 @@ export default class Dnd extends Command {
         }
 
         const pc = await Pc.findById(`${userId}_1`);
-        if (!pc) {
+        if (!pc)
           return message.channel.send("No player character for this user.");
-        }
 
         const embed = new MessageEmbed()
           .setAuthor(user.user.tag, user.user.displayAvatarURL())
@@ -126,3 +90,45 @@ export default class Dnd extends Command {
     }
   }
 }
+
+const formatForMongoQuery = (rawObject) => {
+  const options = {};
+
+  for (let [key, value] of Object.entries(rawObject)) {
+    // if (key !== "bio") {
+    //   value = value.toLowerCase();
+    //   value = value.charAt(0).toUpperCase() + value.slice(1);
+    // }
+
+    // Invalid options are simply dropped.
+    switch (key) {
+      case "name":
+        options.name = value;
+        break;
+      case "race":
+        options.race = value;
+        break;
+      case "class":
+        options.class = value;
+        break;
+      case "pictureURL":
+        options.pictureURL = value;
+        break;
+      case "bio":
+        options.bio = value;
+        break;
+    }
+  }
+
+  return options;
+};
+
+const updatePc = async (id, options) => {
+  try {
+    await Pc.findByIdAndUpdate(id, { $set: options }, { upsert: true }).exec();
+    return true;
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
+};
